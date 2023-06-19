84fe4f33291758e8454639fc57c9e8dc6cd71182
Glenmore Saga Ilagan <38287498+glenmoreilagan@users.noreply.github.com>
2023-06-19 09:18:32 +0800

    update sidenav height and bg

--- src/components/layouts/appLayout.jsx
+++ src/components/layouts/appLayout.jsx
@@ -1,28 +1,28 @@
-'use client'
-import React from 'react'
-import dynamic from 'next/dynamic'
-import Link from 'next/link'
-import { useRouter } from 'next/navigation'
+'use client';
+import React from 'react';
+import dynamic from 'next/dynamic';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 // components
 // import SideNav from './navigation/sideNav'
-import HeadNav from './navigation/headNav'
+import HeadNav from './navigation/headNav';
 
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 
 // https://stackoverflow.com/questions/64622494/usepreventscroll-causes-uselayouteffect-warning-in-nextjs
-const SideNav = dynamic(() => import('./navigation/sideNav'), { ssr: false })
+const SideNav = dynamic(() => import('./navigation/sideNav'), { ssr: false });
 
 export default function AppLayout({ children }) {
-  const router = useRouter()
+  const router = useRouter();
 
   // console.log('AppLayout')
 
   return (
     <>
       <div style={{ display: 'flex', height: '100%' }}>
-        <div style={{ display: 'flex', minHeight: '100vh' }}>
+        <div style={{ display: 'flex' }}>
           <SideNav />
         </div>
         <div className='_content bg-[#FAFAFA]' style={{ width: '100%' }}>
@@ -35,5 +35,5 @@ export default function AppLayout({ children }) {
         </div>
       </div>
     </>
-  )
+  );
 }
