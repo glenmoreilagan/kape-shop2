b10d11b0f362d9f616c251c33aa25571c8d988dc
Glenmore Saga Ilagan <38287498+glenmoreilagan@users.noreply.github.com>
2024-07-12 22:26:23 +0800

    chore: add sample chart for dashboard

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -5,8 +5,45 @@ import React from 'react'
 import AppLayout from '@/components/layouts/AppLayout'
 import BreadcrumbsComponent from '@/components/reusable/Breadcrumbs'
 
+import { Bar, BarChart } from 'recharts'
+
+import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent, ChartLegend, ChartLegendContent } from '@/components/ui/chart'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 import { usersAPI } from '@/components/hooks/users'
 import { logout } from '@/components/hooks/auth'
+
+const chartData = [
+  { month: 'January', desktop: 146, mobile: 80, tablet: 120 },
+  { month: 'February', desktop: 305, mobile: 200, tablet: 50 },
+  { month: 'March', desktop: 237, mobile: 120, tablet: 140 },
+  { month: 'April', desktop: 73, mobile: 190, tablet: 150 },
+  { month: 'May', desktop: 209, mobile: 130, tablet: 120 },
+  { month: 'June', desktop: 214, mobile: 140, tablet: 190 },
+]
+
+const chartConfig = {
+  desktop: {
+    label: 'Desktop',
+    color: "hsl(var(--chart-1))",
+  },
+  mobile: {
+    label: 'Mobile',
+    color: "hsl(var(--chart-2))",
+  },
+  tablet: {
+    label: 'Mobile',
+    color: "hsl(var(--chart-5))",
+  },
+}
 
 export default function IndexDashboard() {
   return (
@@ -19,6 +56,25 @@ export default function IndexDashboard() {
             </BreadcrumbsComponent>
           </div>
         </div>
+
+
+        <Card className="w-[35vw] shadow-none border-none">
+      <CardHeader>
+        <CardTitle>Summary</CardTitle>
+        <CardDescription>These is summary of sale this year</CardDescription>
+      </CardHeader>
+      <CardContent>
+      <ChartContainer config={chartConfig} className='w-[600px]'>
+            <BarChart accessibilityLayer data={chartData}>
+            <ChartTooltip content={<ChartTooltipContent />} />
+            <ChartLegend content={<ChartLegendContent />} />
+              <Bar dataKey='desktop' fill='var(--color-desktop)' radius={4} />
+              <Bar dataKey='mobile' fill='var(--color-mobile)' radius={4} />
+              <Bar dataKey='tablet' fill='var(--color-tablet)' radius={4} />
+            </BarChart>
+          </ChartContainer>
+      </CardContent>
+    </Card>
       </AppLayout>
     </>
   )
