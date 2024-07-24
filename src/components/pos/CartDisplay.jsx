e980a45f003332e68d3abbb5375ff5a5109c345f
Glenmore Saga Ilagan <38287498+glenmoreilagan@users.noreply.github.com>
2024-07-25 01:04:54 +0800

    Update CartDisplay.jsx

--- src/components/pos/CartDisplay.jsx
+++ src/components/pos/CartDisplay.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 
 import {
   Dialog,
@@ -29,14 +29,29 @@ import useCartStore from '@/store/useCartStore'
 
 import { checkout } from '@/components/hooks/sales'
 import { NumberFormatter } from '@/lib/number-formatter'
+import { Input } from '../ui/input'
 
 export default function CartDisplay({ open, setOpen }) {
   const cartItems = useCartStore((state) => state.cart)
   const resetCart = useCartStore((state) => state.resetCart)
 
-  const grandTotal = cartItems?.reduce((prev, current) => {
+  const [cash, setCash] = useState(0)
+
+  const subTotal = cartItems?.reduce((prev, current) => {
     return prev + current.qty * current.price
   }, 0)
+
+  const vat = useMemo(() => {
+    return (parseFloat(subTotal) * 0.12).toFixed(2)
+  }, [subTotal])
+
+  const total = useMemo(() => {
+    return parseFloat(subTotal) + parseFloat(vat)
+  }, [subTotal])
+
+  const creditCash = useMemo(() => {
+    return parseFloat(cash) - parseFloat(total)
+  }, [cash])
 
   const handleCheckout = async (payload) => {
     await checkout({ ...payload, resetCart: resetCart })
@@ -44,35 +59,68 @@ export default function CartDisplay({ open, setOpen }) {
 
   return (
     <>
-      <Sheet open={open} onOpenChange={setOpen}>
+      <Sheet open={open} onOpenChange={setOpen} className='relative'>
         <SheetContent>
           <SheetHeader>
-            <SheetTitle>Cart</SheetTitle>
+            <SheetTitle>
+              Cart <span className='font-thin text-sm'>({cartItems.length})</span>
+            </SheetTitle>
             <SheetDescription>List of items</SheetDescription>
           </SheetHeader>
-          {/* <ScrollArea className='min-h-96'> */}
-          <div>
-            {cartItems?.map((item, index) => {
-              return <CartTable key={item.id} item={item} />
-            })}
-          </div>
-          {/* </ScrollArea> */}
-
-          <div className='mt-3 mb-3'>
-            <div className='flex gap-3 justify-between'>
-              <div className='text-sm'>
-                <p className='inline-block'>Total Items:</p> <span className=''>{cartItems.length}</span>
-              </div>
-              <div className='text-sm'>
-                <p className='inline-block'>Grand Total:</p> <span className=''>{NumberFormatter(grandTotal)}</span>
-              </div>
+          <ScrollArea className='h-[65vh]'>
+            <div>
+              {cartItems?.map((item, index) => {
+                return <CartTable key={item.id} item={item} />
+              })}
             </div>
-            <div></div>
-          </div>
+          </ScrollArea>
+
           <SheetFooter>
-            <Button disabled={cartItems.length <= 0} onClick={() => handleCheckout({ payload: cartItems, setOpen })}>
-              Checkout
-            </Button>
+            <div className='px-6 absolute bottom-5 w-full left-0'>
+              <div className='flex gap-3 justify-end mb-3'>
+                <div className='text-sm'>
+                  <div className='flex gap-3 justify-between'>
+                    <p className='inline-block'>Sub Total:</p>
+                    <span className='font-bold'>{parseInt(subTotal) > 0 ? NumberFormatter(subTotal) : '-'}</span>
+                  </div>
+                  <div className='flex gap-3 justify-between'>
+                    <p className='inline-block'>Vat:</p>
+                    <span className='font-bold'>{parseInt(vat) > 0 ? NumberFormatter(parseFloat(vat)) : '-'}</span>
+                  </div>
+                  <div className='flex gap-3 justify-between'>
+                    <p className='inline-block'>Cash:</p>
+                    <span className='font-bold'>{parseInt(cash) > 0 ? NumberFormatter(cash) : '-'}</span>
+                  </div>
+                  <div className='flex gap-3 justify-between'>
+                    <p className='inline-block'>Grand Total:</p>
+                    <span className='font-bold'>{parseInt(total) > 0 ? NumberFormatter(total) : '-'}</span>
+                  </div>
+                  <div className='flex gap-3 justify-between'>
+                    <p className='inline-block'>Credit:</p>
+                    <span className='font-bold'>{parseInt(creditCash) > 0 ? NumberFormatter(creditCash) : '-'}</span>
+                  </div>
+                </div>
+              </div>
+
+              <Input
+                placeholder='Cash'
+                className='mb-3'
+                value={cash}
+                onChange={(e) => {
+                  const value = e.target.value
+                  const regex = /[^0-9.]/g // Regex to match any character that is not a number or dot
+                  const filteredValue = value.replace(regex, '')
+                  setCash((prev) => filteredValue)
+                }}
+              />
+              <Button
+                className='w-full'
+                disabled={cartItems.length <= 0}
+                onClick={() => handleCheckout({ payload: cartItems, setOpen })}
+              >
+                Checkout
+              </Button>
+            </div>
           </SheetFooter>
         </SheetContent>
       </Sheet>
