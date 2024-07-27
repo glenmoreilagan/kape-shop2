4f6193153a2a0c8547c0e15aef11092a046f9997
Glenmore Saga Ilagan <38287498+glenmoreilagan@users.noreply.github.com>
2024-07-27 23:45:32 +0800

    modified:   src/components/pos/CartDisplay.jsx

--- src/components/pos/CartDisplay.jsx
+++ src/components/pos/CartDisplay.jsx
@@ -58,7 +58,7 @@ export default function CartDisplay({ open, setOpen }) {
   const handleCheckout = async (payload) => {
     try {
       const response = await checkout({ ...payload, resetCart: resetCart })
-      // toast.success(<MessageAlert header='Success!' body='Checkout Success.' />)
+      toast.success(<MessageAlert header='Success!' body='Checkout Success.' />)
     } catch (error) {
       console.log(error)
     }
