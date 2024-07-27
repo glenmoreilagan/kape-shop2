8027dad51175429dd6f10004a467bcede41c927c
Glenmore Saga Ilagan <38287498+glenmoreilagan@users.noreply.github.com>
2024-07-27 23:35:13 +0800

    modified:   src/components/hooks/sales.js

--- src/components/hooks/sales.js
+++ src/components/hooks/sales.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { useQuery } from '@tanstack/react-query'
 import newAxios from '@/lib/new-axios'
 
-export async function checkout({ ...payload }) {
+export async function checkout({ payload }) {
   try {
-    const response = await newAxios.post('/api/sales', { data: payload })
+    const response = await newAxios.post('/api/sales', { data: { ...payload } })
     return response.data
   } catch (error) {
     return 'Something went wrong.'
